15f1f0a775a9d84de5c79cdc6d970d46b864c151
David Bautista <ds.bautista1998@gmail.com>
2020-10-20 12:30:45 -0500

    Plot 1

--- plot1.R
+++ plot1.R
@@ -1,6 +1,8 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 
+mean <- tapply(NEI$Emissions, NEI$year, mean)
+
 png("plot1.png")
-with(NEI, plot(year, Emissions, pch = 19, col=year, 
-               xlab="Year", ylab = "Emissions"))
+plot(names(mean), mean, type = "l", xlab="Year", ylab="emissions",
+     main="Mean of emissions 1999, 2002, 2005, 2008")
 dev.off()
